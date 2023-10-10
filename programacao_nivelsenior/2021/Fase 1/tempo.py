244b90acb4fd18cb618b527558c8c9b5658802cd
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-10-10 14:37:49 -0300

    Tentativa

--- programacao_nivelsenior/2021/Fase 1/tempo.py
+++ programacao_nivelsenior/2021/Fase 1/tempo.py
@@ -7,10 +7,12 @@ for e in range(n):
 tempo_resposta_usuario = 
 {
     '2':{
+        par_ok: False
         par_evento: ['R', 'E'],
         tempo: 12
     }
     '45':{
+        par_ok: False
         par_evento: ['R', 'E'],
         tempo: 12
     }
@@ -33,6 +35,7 @@ for e in eventos:
                 tempo_resposta_usuario.update(
                     {
                         usuario:{
+                            'par_ok':False,
                             'par_evento':par_evento,
                             'tempo':tempo
                         }
@@ -42,6 +45,7 @@ for e in eventos:
             tempo_resposta_usuario.update(
             {
                 e[1]:{
+                    'par_ok':False,
                     'par_evento': [e[0]],
                     'tempo': 1
                 }
@@ -51,8 +55,24 @@ for e in eventos:
         if e[0] == 'T':
             for u in tempo_resposta_usuario.items():
                 usuario = u[0]
-                par_evento = u[1]['par_evento']
-                tempo = u[1]['tempo']+int(e[1])
+                par_ok = u[1]['par_ok']
+                par_evento = u[2]['par_evento']
+                tempo = u[3]['tempo']+int(e[1])
+                tempo_resposta_usuario.update(
+                    {
+                        usuario:{
+                            'par_ok':par_ok,
+                            'par_evento':par_evento,
+                            'tempo':tempo
+                        }
+                    }
+                )
+        else:
+            for u in tempo_resposta_usuario.items():
+                usuario = u[0]
+                par_ok = u[1]['par_ok']
+                par_evento = u[2]['par_evento']
+                tempo = u[3]['tempo']+1
                 tempo_resposta_usuario.update(
                     {
                         usuario:{
@@ -61,15 +81,6 @@ for e in eventos:
                         }
                     }
                 )
-        else:
-            tempo_resposta_usuario.update(
-            {
-                e[1]:{
-                    'par_evento': [e[0]],
-                    'tempo': 1
-                }
-            }
-        )
 
 
 print(tempo_resposta_usuario)
