53df7cc81e7eee5dc8d6cc001de5391eb94605f7
Marcos Fellipe Guimarães <marcosfgui2006@gmail.com>
2023-10-16 15:00:56 -0300

    Fase 2 2019 NJ

--- programacao_niveljunior/2019/Fase 2/tabela.py
+++ programacao_niveljunior/2019/Fase 2/tabela.py
@@ -4,7 +4,14 @@ if j == -1:
     if v != -1 and e != -1 and d != -1:
         j = v+e+d
     else:
-        ...
+        if v == -1 and e != -1 and p != -1:
+            v = (p-e)/3
+        if e == -1 and p != -1 and v != -1:
+            e=p-3*v
+            if e < -1:
+                e=e*(-1)
+        if d != -1:
+            j=v+e+d
 if v == -1:
     if j != -1 and e != -1 and d != -1:
         v=j-e-d
@@ -18,4 +25,4 @@ if p == -1:
     if v != -1 and e != -1:
         p = 3*v+e
 
-print(j,p,v,e,d)
+print(int(j),int(p),int(v),int(e),int(d))
